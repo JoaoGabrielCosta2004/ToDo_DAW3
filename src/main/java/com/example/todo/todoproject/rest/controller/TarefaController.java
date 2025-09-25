5c67d24e183aa8e1399f35abf6a981c7f921ff66
João Gabriel Costa <joaogabrielcosta2004@gmail.com>
2025-09-25 16:43:06 -0300

    Creating project

---
+++ src/main/java/com/example/todo/todoproject/rest/controller/TarefaController.java
@@ -0,0 +1,14 @@
+package com.example.todo.todoproject.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tarefas")
+public class TarefaController {
+    @GetMapping
+    public void getAll(){
+        
+    }
+}
