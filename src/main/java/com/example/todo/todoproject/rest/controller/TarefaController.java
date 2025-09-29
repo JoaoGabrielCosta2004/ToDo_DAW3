afe9cfbf2d29f43dcb3b563dae4bb246c1242551
João Gabriel Costa <joaogabrielcosta2004@gmail.com>
2025-09-29 14:31:03 -0300

    feat: Marcações do controller

--- src/main/java/com/example/todo/todoproject/rest/controller/TarefaController.java
+++ src/main/java/com/example/todo/todoproject/rest/controller/TarefaController.java
@@ -1,14 +1,41 @@
 package com.example.todo.todoproject.rest.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.todo.todoproject.service.TarefaService;
 
 @RestController
 @RequestMapping("/tarefas")
 public class TarefaController {
+    private final TarefaService service;
+    public TarefaController(TarefaService service){
+        this.service = service;
+    }
     @GetMapping
     public void getAll(){
         
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void salvar(){
+
+    }
+
+    @PutMapping
+    public void atualizar(){
+
+    }
+
+    @DeleteMapping
+    public void remover(){
+
     }
 }
---
+++ src/main/java/com/example/todo/todoproject/service/TarefaService.java
@@ -0,0 +1,5 @@
+package com.example.todo.todoproject.service;
+
+public class TarefaService {
+
+}
